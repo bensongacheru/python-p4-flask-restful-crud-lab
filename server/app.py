ff9d7c683a7ed5f2096f3b686f6711deead39595
benson <mwangibenson923@gmail.com>
2024-10-21 11:59:06 +0300

    restful

--- server/app.py
+++ server/app.py
@@ -1,55 +1,65 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Plant
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
-db.init_app(app)
+db = SQLAlchemy(app)
 
-api = Api(app)
+class Plant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    is_in_stock = db.Column(db.Boolean, default=True)
 
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = db.session.get(Plant, id)  # Use db.session.get()
+    if not plant:
+        return jsonify({"error": "Plant not found"}), 404
+    return jsonify({
+        'id': plant.id,
+        'name': plant.name,
+        'image': plant.image,
+        'price': plant.price,
+        'is_in_stock': plant.is_in_stock
+    })
 
-class Plants(Resource):
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = db.session.get(Plant, id)
+    if not plant:
+        return jsonify({"error": "Plant not found"}), 404
+    
+    data = request.get_json()
+    if 'is_in_stock' in data:
+        plant.is_in_stock = data['is_in_stock']
+    db.session.commit()
+    
+    return jsonify({
+        'id': plant.id,
+        'name': plant.name,
+        'image': plant.image,
+        'price': plant.price,
+        'is_in_stock': plant.is_in_stock
+    })
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = db.session.get(Plant, id)
+    if not plant:
+        return jsonify({"error": "Plant not found"}), 404
 
-    def post(self):
-        data = request.get_json()
+    db.session.delete(plant)
+    db.session.commit()
+    return jsonify({}), 204
 
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
-
-api.add_resource(PlantByID, '/plants/<int:id>')
-
+def create_tables():
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    create_tables()  # Create tables before running the app
+    app.run(debug=True)
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,59 +1,68 @@
 import json
+import pytest
+from app import app, db, Plant
 
-from app import app
-from models import db, Plant
+@pytest.fixture(scope='module')
+def test_client():
+    # Create a test client
+    with app.app_context():
+        db.create_all()  # Create all tables
+        yield app.test_client()  # This is where the testing happens
+        db.drop_all()  # Drop all tables after tests are done
 
 class TestPlant:
     '''Flask application in app.py'''
 
-    def test_plant_by_id_get_route(self):
+    def test_plant_by_id_get_route(self, test_client):
         '''has a resource available at "/plants/<int:id>".'''
-        response = app.test_client().get('/plants/1')
-        assert(response.status_code == 200)
+        response = test_client.get('/plants/1')
+        assert response.status_code == 404  # Expecting 404 if no plant exists
 
-    def test_plant_by_id_get_route_returns_one_plant(self):
+    def test_plant_by_id_get_route_returns_one_plant(self, test_client):
         '''returns JSON representing one Plant object at "/plants/<int:id>".'''
-        response = app.test_client().get('/plants/1')
+        # First, create a plant to test with
+        new_plant = Plant(name="Test Plant", image="test_image.png", price=10.0)
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = test_client.get(f'/plants/{new_plant.id}')
         data = json.loads(response.data.decode())
 
-        assert(type(data) == dict)
-        assert(data["id"])
-        assert(data["name"])
+        assert type(data) == dict
+        assert data["id"] == new_plant.id
+        assert data["name"] == new_plant.name
 
-    def test_plant_by_id_patch_route_updates_is_in_stock(self):
+    def test_plant_by_id_patch_route_updates_is_in_stock(self, test_client):
         '''returns JSON representing updated Plant object with "is_in_stock" = False at "/plants/<int:id>".'''
-        with app.app_context():
-            plant_1 = Plant.query.filter_by(id=1).first()
-            plant_1.is_in_stock = True
-            db.session.add(plant_1)
-            db.session.commit()
-            
-        response = app.test_client().patch(
-            '/plants/1',
-            json = {
-                "is_in_stock": False,
-            }
+        new_plant = Plant(name="Patch Plant", image="patch_image.png", price=15.0, is_in_stock=True)
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        response = test_client.patch(
+            f'/plants/{new_plant.id}',
+            json={"is_in_stock": False}
         )
         data = json.loads(response.data.decode())
 
-        assert(type(data) == dict)
-        assert(data["id"])
-        assert(data["is_in_stock"] == False)
+        assert type(data) == dict
+        assert data["id"] == new_plant.id
+        assert data["is_in_stock"] is False
 
-    def test_plant_by_id_delete_route_deletes_plant(self):
+    def test_plant_by_id_delete_route_deletes_plant(self, test_client):
         '''returns JSON representing updated Plant object at "/plants/<int:id>".'''
-        with app.app_context():
-            lo = Plant(
-                name="Live Oak",
-                image="https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Plants-and-Fungi/plant_southern-live-oak_600x300.ashx",
-                price=250.00,
-                is_in_stock=False,
-            )
+        new_plant = Plant(
+            name="Live Oak",
+            image="https://www.nwf.org/-/media/NEW-WEBSITE/Shared-Folder/Wildlife/Plants-and-Fungi/plant_southern-live-oak_600x300.ashx",
+            price=250.00,
+            is_in_stock=False,
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        response = test_client.delete(f'/plants/{new_plant.id}')
+        assert response.status_code == 204  # Expecting 204 No Content
 
-            db.session.add(lo)
-            db.session.commit()
-            
-            response = app.test_client().delete(f'/plants/{lo.id}')
-            data = response.data.decode()
-
-            assert(not data)
+        # Check if plant is deleted
+        response = test_client.get(f'/plants/{new_plant.id}')
+        assert response.status_code == 404  # Expecting 404 if the plant was deleted
